a0db3fd4fccb31ecd27055ea120ba80a3c39aa4c
danyy <dany@gmail.com>
2022-01-03 21:34:38 -0600

    Validando el Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,10 +1,13 @@
+import { useEffect, useState } from "react";
 import styled from "@emotion/styled";
 import useSelectMonedas from "../hooks/useSelectMonedas";
+import { monedas } from "../data/monedas";
 
 const InputSubmit = styled.input`
     background-color: #9497ff;
     border: none;
     width: 100%;
+    margin-top: 30px;
     padding: 10px;
     color: #fff;
     font-weight: 700;
@@ -20,13 +23,43 @@ const InputSubmit = styled.input`
 `
 
 const Formulario = () => {
-    
-    const [ selectMonedas ] = useSelectMonedas();
 
-    selectMonedas();
+    const [criptos, setCriptos] = useState([]);
+    
+    const [ moneda, SelectMonedas ] = useSelectMonedas('Elige tu Moneda', monedas);
+    const [ criptomoneda, SelectCriptomonedas ] = useSelectMonedas('Elige tu Criptomoneda', criptos);
+
+    useEffect(() => {
+
+        const consultarApi = async () => {
+    
+            const url = `https://min-api.cryptocompare.com/data/top/mktcapfull?limit=20&tsym=USD`;
+            const respuesta = await fetch(url);
+            const resultado = await respuesta.json();
+
+            const arrayCriptos = resultado.Data.map( cripto => {
+
+                const objeto = {
+                    id: cripto.CoinInfo.Name,
+                    nombre: cripto.CoinInfo.FullName
+                }
+
+                return objeto;
+            });
+
+            setCriptos(arrayCriptos);
+        }
+        consultarApi();
+    
+    }, [])
 
     return (
         <form>
+
+            <SelectMonedas />
+
+            <SelectCriptomonedas />
+
             <InputSubmit type="submit" value="Cotizar"/>
         </form>
     )
--- src/hooks/useSelectMonedas.jsx
+++ src/hooks/useSelectMonedas.jsx
@@ -1,12 +1,52 @@
-import React from 'react'
+import {useState} from 'react';
+import styled from '@emotion/styled';
 
-const useSelectMonedas = () => {
+const Label = styled.label`
+    display: block;
+    font-family: 'Lato', sans-serif;
+    color: #fff;
+    font-size: 24px;
+    font-weight: 700;
+    margin: 15px 0;
+`;
 
-    const selectMonedas = () => {
-        console.log('Desde Select Monedas');
-    }
+const Select = styled.select`
+    width: 100%;
+    font-size: 18px;
+    padding: 14px;
+    border-radius: 10px;
+`;
 
-    return [ selectMonedas ]
+const useSelectMonedas = (label, opciones) => {
+
+    const [state, setState] = useState('');
+
+    const SelectMonedas = () => (
+
+        <>
+            <Label>{label}</Label>
+
+            <Select
+                value={state}
+                onChange={e => setState(e.target.value)}
+            >
+
+                <option value="">-- Seleccione --</option>
+
+                {opciones.map(opcion => (
+                    <option 
+                        key={opcion.id}
+                        value={opcion.id}
+                    >{opcion.nombre}</option>
+                ))}
+
+            </Select>
+
+        </>
+
+    )
+
+    return [ state, SelectMonedas ]
 }
 
 
